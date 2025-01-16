10accdb3ccfed5380b9cfb969c41f6e0c45ef7dd
Henrique12340875 <henriquegames1409@gmail.com>
2025-01-16 20:53:51 -0300

    feat: basic crud fo users(Late modificated)

--- app.rb
+++ app.rb
@@ -2,33 +2,70 @@ require 'sinatra'
 require 'sinatra/activerecord'
 require 'json'
 require 'dotenv'
+require_relative 'models/Users'
 
 Dotenv.load
 
-class MainApp < Sinatra::Base
-    register Sinatra::ActiveRecordExtension
+set :database,{adapter:"sqlite3", database:"./db/development.sqlite3"}
 
-    set :database,{adapter:"sqlite3", database:"./db/development.sqlite3"}
-    set :port,ENV["PORT"]
+set :port, ENV.fetch("PORT", 3000)
 
-    get "/users/:id" do
-
-    end
-    
-    get "/users" do
-    
-    end
-    
-    post "/users" do
-    
-    end
-    
-    put "/users/:id" do
-    
-    end
-    
-    delete "/users/:id" do
-    
+get "/users/:id" do
+    begin
+        user = User.find(params['id'])
+        user.to_json
+    rescue ActiveRecord::RecordNotFound => e
+        {
+            erro: e.message
+        }.to_json
     end
 end
+        
+get "/users" do
+    users = User.all
+
+    users.to_json
+end
+        
+post "/users" do
+    begin
+        body = JSON.parse(request.body.read) 
+        user = User.create(name:body['name'],email:body['email'],password:body['password']) 
+        user.to_json 
+    rescue ActiveRecord::RecordNotSaved => e
+        {
+            erro: e.message
+        }.to_json
+    end
+end
+        
+put "/users/:id" do
+    begin
+        id = params['id']
+        body = JSON.parse(request.body.read)
+        user = User.find(id)
+        user.update(name:body['name'],email:body['email'],password:body['password'])
+        user.to_json
+    rescue ActiveRecord::RecordNotSaved => e
+        {
+            erro: e.message
+        }.to_json
+    end
+end
+        
+delete "/users/:id" do
+    begin
+        user = User.find(params['id'])
+        user.destroy
+
+        user.to_json
+    rescue ActiveRecord::RecordNotDestroyed => e
+        {
+            erro: e.message
+        }.to_json
+    end    
+end
+
+
+
 
